7fd77f285bc1549b3b445905206a6ff898191bac
HenryArin <128102839+HenryArin@users.noreply.github.com>
2026-03-03 07:13:17 -0800

    feat: implement player movement with global key input system

--- defs.h
+++ defs.h
@@ -3,25 +3,50 @@
 
 typedef double Flt;
 typedef double Vec[3];
-typedef Flt	Matrix[4][4];
+typedef Flt Matrix[4][4];
 
-#define MakeVector(x, y, z, v) (v)[0]=(x),(v)[1]=(y),(v)[2]=(z)
-#define VecNegate(a)	(a)[0]=(-(a)[0]); (a)[1]=(-(a)[1]); (a)[2]=(-(a)[2]);
-#define VecDot(a,b)	((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
-#define VecLen(a)	((a)[0]*(a)[0]+(a)[1]*(a)[1]+(a)[2]*(a)[2])
-#define VecLenSq(a)	sqrtf((a)[0]*(a)[0]+(a)[1]*(a)[1]+(a)[2]*(a)[2])
-#define VecCopy(a,b)	(b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2];
-#define VecAdd(a,b,c) (c)[0]=(a)[0]+(b)[0]; (c)[1]=(a)[1]+(b)[1]; (c)[2]=(a)[2]+(b)[2]
-#define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; (c)[1]=(a)[1]-(b)[1]; (c)[2]=(a)[2]-(b)[2]
-#define VecS(A,a,b) (b)[0]=(A)*(a)[0]; (b)[1]=(A)*(a)[1]; (b)[2]=(A)*(a)[2]
-#define VecAddS(A,a,b,c) (c)[0]=(A)*(a)[0]+(b)[0]; (c)[1]=(A)*(a)[1]+(b)[1]; (c)[2]=(A)*(a)[2]+(b)[2]
-#define VecCross(a,b,c) (c)[0]=(a)[1]*(b)[2]-(a)[2]*(b)[1]; (c)[1]=(a)[2]*(b)[0]-(a)[0]*(b)[2]; (c)[2]=(a)[0]*(b)[1]-(a)[1]*(b)[0]
-#define VecZero(v) (v)[0]=0.0;(v)[1]=0.0;v[2]=0.0
-#define ABS(a) (((a)<0)?(-(a)):(a))
-#define SGN(a) (((a)<0)?(-1):(1))
-#define SGND(a) (((a)<0.0)?(-1.0):(1.0))
+#define MakeVector(x, y, z, v) (v)[0] = (x), (v)[1] = (y), (v)[2] = (z)
+#define VecNegate(a)    \
+	(a)[0] = (-(a)[0]); \
+	(a)[1] = (-(a)[1]); \
+	(a)[2] = (-(a)[2]);
+#define VecDot(a, b) ((a)[0] * (b)[0] + (a)[1] * (b)[1] + (a)[2] * (b)[2])
+#define VecLen(a) ((a)[0] * (a)[0] + (a)[1] * (a)[1] + (a)[2] * (a)[2])
+#define VecLenSq(a) sqrtf((a)[0] * (a)[0] + (a)[1] * (a)[1] + (a)[2] * (a)[2])
+#define VecCopy(a, b) \
+	(b)[0] = (a)[0];  \
+	(b)[1] = (a)[1];  \
+	(b)[2] = (a)[2];
+#define VecAdd(a, b, c)       \
+	(c)[0] = (a)[0] + (b)[0]; \
+	(c)[1] = (a)[1] + (b)[1]; \
+	(c)[2] = (a)[2] + (b)[2]
+#define VecSub(a, b, c)       \
+	(c)[0] = (a)[0] - (b)[0]; \
+	(c)[1] = (a)[1] - (b)[1]; \
+	(c)[2] = (a)[2] - (b)[2]
+#define VecS(A, a, b)      \
+	(b)[0] = (A) * (a)[0]; \
+	(b)[1] = (A) * (a)[1]; \
+	(b)[2] = (A) * (a)[2]
+#define VecAddS(A, a, b, c)         \
+	(c)[0] = (A) * (a)[0] + (b)[0]; \
+	(c)[1] = (A) * (a)[1] + (b)[1]; \
+	(c)[2] = (A) * (a)[2] + (b)[2]
+#define VecCross(a, b, c)                       \
+	(c)[0] = (a)[1] * (b)[2] - (a)[2] * (b)[1]; \
+	(c)[1] = (a)[2] * (b)[0] - (a)[0] * (b)[2]; \
+	(c)[2] = (a)[0] * (b)[1] - (a)[1] * (b)[0]
+#define VecZero(v) \
+	(v)[0] = 0.0;  \
+	(v)[1] = 0.0;  \
+	v[2] = 0.0
+#define ABS(a) (((a) < 0) ? (-(a)) : (a))
+#define SGN(a) (((a) < 0) ? (-1) : (1))
+#define SGND(a) (((a) < 0.0) ? (-1.0) : (1.0))
 
-typedef struct t_rect {
+typedef struct t_rect
+{
 	int left;
 	int top;
 	int right;
@@ -33,30 +58,35 @@ typedef struct t_rect {
 	int centery;
 } Rect;
 
-typedef struct t_mouse {
-	int x,y;
-	int lastx,lasty;
+typedef struct t_mouse
+{
+	int x, y;
+	int lastx, lasty;
 	int lbuttondown;
 } Mouse;
 
-typedef struct t_bsphere {
+typedef struct t_bsphere
+{
 	Vec center;
 	Flt radius;
 	Flt radiusSqr;
 } Bsphere;
 
-typedef struct t_bbox {
+typedef struct t_bbox
+{
 	Flt min[3];
 	Flt max[3];
 } Bbox;
 
-typedef struct t_texmap {
+typedef struct t_texmap
+{
 	int xres, yres;
 	unsigned char *c;
 	unsigned int *i;
 } Texmap;
 
-typedef struct t_screen {
+typedef struct t_screen
+{
 	int x_res, y_res;
 	int screen_x_res, screen_y_res;
 	float gl_perspective_viewangle;
@@ -65,7 +95,8 @@ typedef struct t_screen {
 } Screen;
 
 #define MAX_LIGHTS (4)
-typedef struct t_lights {
+typedef struct t_lights
+{
 	int onoff;
 	int nlights;
 	Vec light;
@@ -74,11 +105,13 @@ typedef struct t_lights {
 	float glPos[4];
 } Lights;
 
-typedef struct t_keys {
+typedef struct t_keys
+{
 	int autorepeatstate, shiftstate, ctrlstate, shiftlockstate;
 } Keys;
 
-typedef struct t_camera {
+typedef struct t_camera
+{
 	Vec pos;
 	Vec move;
 	Vec dir;
@@ -88,13 +121,15 @@ typedef struct t_camera {
 	Matrix m;
 } Camera;
 
-typedef struct t_mats {
+typedef struct t_mats
+{
 	Matrix worldSpace;
 	Matrix cameraSpace;
 	Matrix playerSpace;
 } Mats;
 
-typedef struct t_frustum {
+typedef struct t_frustum
+{
 	Flt frontPlaneDistance;
 	Vec frontPlaneNormal;
 	Vec frontPlanePoint;
@@ -103,12 +138,12 @@ typedef struct t_frustum {
 	Flt farObjectPlaneDistance;
 	Vec farObjectPlanePoint;
 	Flt missile_view_distance2;
-	//#1 is left   plane normal
-	//#2 is right  plane normal
-	//#3 is top    plane normal
-	//#4 is bottom plane normal
-	//#5 is front  plane normal
-	//#6 is far    plane normal
+	// #1 is left   plane normal
+	// #2 is right  plane normal
+	// #3 is top    plane normal
+	// #4 is bottom plane normal
+	// #5 is front  plane normal
+	// #6 is far    plane normal
 	Vec planes[6];
 	Flt glplanesd[6][4];
 	float glplanesf[6][4];
@@ -117,5 +152,7 @@ typedef struct t_frustum {
 	Vec save_planes[6];
 } Frustum;
 
-#endif
+// Global input
+extern char g_keys[65536];
 
+#endif
--- harinaga.cpp
+++ harinaga.cpp
@@ -1,3 +1,73 @@
-void initPlayer() {}
-void updatePlayer() {}
-void drawPlayer() {}
+#include "harinaga.h"
+#include <GL/gl.h>
+#include <cmath>
+#include "defs.h"
+#include <X11/keysym.h>
+
+static float px = 100.0f;
+static float py = 100.0f;
+static float speed = 4.0f;
+
+void initPlayer()
+{
+    px = 100.0f;
+    py = 100.0f;
+    speed = 4.0f;
+}
+
+void updatePlayer()
+{   
+    // keys for player movement
+    if (g_keys[XK_Left] || g_keys[XK_a])
+    {
+        px -= speed;
+    }
+    if (g_keys[XK_Right] || g_keys[XK_d])
+    {
+        px += speed;
+    }
+    if (g_keys[XK_Up] || g_keys[XK_w])
+    {
+        py += speed;
+    }
+    if (g_keys[XK_Down] || g_keys[XK_s])
+    {
+        py -= speed;
+    }
+
+    // clamp player movement to the window boundaries
+    if (px < 0.0f)
+    {
+        px = 0.0f;
+    }
+    if (px > 500.0f)
+    {
+        px = 500.0f;
+    }
+    if (py < 0.0f)
+    {
+        py = 0.0f;
+    }
+    if (py > 650.0f)
+    {
+        py = 650.0f;
+    }
+}
+
+void drawPlayer()
+{
+    float w = 20.0f;
+    float h = 20.0f;
+
+    glPushMatrix();
+    glTranslatef(px, py, 0.0f);
+
+    glBegin(GL_QUADS);
+    glVertex2f(-w, -h);
+    glVertex2f(-w, h);
+    glVertex2f(w, h);
+    glVertex2f(w, -h);
+    glEnd();
+
+    glPopMatrix();
+}
